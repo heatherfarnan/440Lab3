886c9cad138c2c8d8e299dd408ed02c04c50477b
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-10-06 20:21:31 +0000

    iteration 1

---
+++ joystick.py
@@ -0,0 +1,12 @@
+from PCF8591 import PCF8591
+
+class Joystick:
+
+  def __init__ (self, address):
+    self.PCF = PCF8591(address)
+
+  def getX(self):
+    return self.OCF.read(1)
+
+  def getY(self):
+    return self.PCF.real(2)
